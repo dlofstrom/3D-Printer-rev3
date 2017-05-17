5369a34e51303945fa7af8abc33083ee39ac8759
dlofstrom <lofstrom.daniel@gmail.com>
2017-05-17 19:09:08 +0200

    Code for G-code functionality generated and a small test program for testing command parsing and handling

---
+++ code/gcode/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "gcode.h"
+
+int main() {
+    printf("Testing G-code parser!\n");
+
+    gcode_parse("G1 X100 Y100 Z100\n");
+    gcode_parse("N3 T0*57 ; This is a comment\n");
+    gcode_parse("G\n");
+    gcode_parse("G X100\n");
+    
+    return 1;
+}
