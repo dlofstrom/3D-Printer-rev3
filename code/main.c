c776fddaeb1531091fe21cc11563e42b587f5e37
dlofstrom <lofstrom.daniel@gmail.com>
2017-06-01 19:35:26 +0200

    First blink example on assembled PCB

--- code/main.c
+++ code/main.c
@@ -6,13 +6,13 @@
 
 int main(void)
 {
-    nrf_gpio_pin_dir_set(21, NRF_GPIO_PIN_DIR_OUTPUT);
+    nrf_gpio_pin_dir_set(11, NRF_GPIO_PIN_DIR_OUTPUT);
   
     while (true)
     {
-        nrf_gpio_pin_set(21);
+        nrf_gpio_pin_set(11);
         nrf_delay_ms(500);
-        nrf_gpio_pin_clear(21);
+        nrf_gpio_pin_clear(11);
         nrf_delay_ms(500);
     }
 }
